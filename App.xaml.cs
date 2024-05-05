837f0ce04d4998cbfe4917fe501790a0dcf9c373
Dragonfury1928 <42259477+Dragonfury1928@users.noreply.github.com>
2024-05-04 21:16:32 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AwesomeWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
